0e524e6ce4324f07af387b7ba2ba5e3e1308339b
Kirill Kurentsov <immortalsteel@gmail.com>
2025-05-04 19:26:17 +0300

    fix: flas message issue on successful page submit

--- src/Config/Middleware.php
+++ src/Config/Middleware.php
@@ -7,6 +7,10 @@ namespace App\Config;
 use Slim\App;
 use Psr\Container\ContainerInterface;
 use App\Middleware\ErrorHandlerMiddleware;
+use Slim\Flash\Messages;
+use Slim\Views\Twig;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 class Middleware
 {
@@ -25,6 +29,15 @@ class Middleware
             $settings['logErrors'],
             $settings['logErrorDetails']
         );
+
+        // Add Flash middleware to refresh flash messages for each request
+        $app->add(function (Request $request, RequestHandler $handler) use ($container) {
+            $twig = $container->get(Twig::class);
+            $flash = $container->get(Messages::class);
+            $twig->getEnvironment()->addGlobal('flash', $flash->getMessages());
+
+            return $handler->handle($request);
+        });
 
         // Add Twig Middleware using the factory
         $twigMiddlewareFactory = $container->get('twig-middleware-factory');
